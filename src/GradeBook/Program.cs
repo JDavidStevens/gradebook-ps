5ce565b61106a1e8354db71df27ebf402c9d54e5
David Stevens <james.stevens@healthcatalyst.com>
2020-07-02 16:04:01 -0600

    initial commit with content

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book();
+            book.addGrade(103.5);
+
+            var grades = new List<double>() {79, 82, 0.4, 0.7, 1};
+            grades.Add(0.3);
+
+            var result = 0.0;
+            foreach(var number in grades)
+            {
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N3}");
+        }
+    }
+}
